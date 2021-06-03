49ca5b2efc6f8008020a8f578048686f7a9eac8f
mie <gawow11@gmail.com>
2021-06-03 17:17:05 +0900

    :+1: modify header of reply email

--- confirm.php
+++ confirm.php
@@ -15,15 +15,26 @@
     if($_SERVER['REQUEST_METHOD'] === 'POST') {
         //送信ボタンが押されたら、問い合わせ確認メールを送信する
 
+        //文字化け防止
+        mb_language("Japanese");
+        mb_internal_encoding("UTF-8");
+        $from_name = mb_encode_mimeheader("株式会社〇〇〇〇");
+        $from_address = 'info@example.com';
+
+        //セパレートチェックボックスアイテム
         $check = implode( ', ', $post['check'] );
 
         //自動返信
         $to1 = $post['email'];
-        $from1 = 'info@example.com';
+        $from1 = "From: ".$from_name.$from_address;
         $subject1 = 'お問い合わせありがとうございます';
         $body1 = <<<EOT
         ※このメールはシステムからの自動返信です。
 
+        株式会社〇〇〇〇へのお問合せありがとうございました。
+        以下の内容で送信いたしました。
+        ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
         名前： {$post['name']}
         ふりがな： {$post['kana']}
         メールアドレス： {$post['email']}
@@ -35,14 +46,29 @@
         好きなお寿司： {$check}
         今日の天気： {$post['radio']}
         問い合わせ内容： {$post['message']}
+
+        ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+        後日担当者よりご連絡いたします。
+        よろしくお願いいたします。
+
+        ================================
+        株式会社〇〇〇〇
+        〒444-4444
+        静岡県浜松市中区0000
+        000-123-456
+        ================================
         EOT;
 
 
-        //担当者宛
-        $to2 = 'info@example.com';
-        $from2 = $post['email'];
-        $subject2 = 'お問い合わせが届きました';
+        //受付メール
+        $to2 = 'gawow11@gmail.com';
+        $from2 = 'From: '.$post['email'];
+        $subject2 = 'ホームページよりお問合せを受け付けました。';
         $body2 = <<<EOT
+        お問い合わせ内容
+        ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
         名前： {$post['name']}
         ふりがな： {$post['kana']}
         メールアドレス： {$post['email']}
@@ -54,6 +80,8 @@
         好きなお寿司： {$check}
         今日の天気： {$post['radio']}
         問い合わせ内容： {$post['message']}
+
+        ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
         EOT;
 
         // var_dump($body);
@@ -61,8 +89,8 @@
         mb_language("Japanese");
         mb_internal_encoding("UTF-8");
 
-        mb_send_mail($to1, $subject1, $body1, 'From: {$from1}');
-        mb_send_mail($to2, $subject2, $body2, 'From: {$from2}');
+        mb_send_mail($to1, $subject1, $body1, $from1);
+        mb_send_mail($to2, $subject2, $body2, $from2);
 
         //セッションを消してお礼画面へ
         unset($_SESSION['form']);
@@ -135,7 +163,7 @@
                 </div>
                 <a class="form__button button-gray" href="index.php">入力内容を変更する</a>
                 <button type="submit" class="form__button button-blue">送信する</button>
-            <form action="" method="POST">
+            </form>
         </div>
     </main>
 </body>
