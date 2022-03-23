30b1ef711c02054fcc305b78cc6d4926fae9345e
suusan11 <gawow11@gmail.com>
2022-03-23 15:11:37 +0900

    :raised_hands: update contact form

--- confirm.php
+++ confirm.php
@@ -9,7 +9,7 @@
         exit();
     }else {
         //indexで渡されたフォームの内容を再度変数に入れる
-        $post = $_SESSION['form'];
+        $_POST = $_SESSION['form'];
     }
 
     if($_SERVER['REQUEST_METHOD'] === 'POST') {
@@ -22,10 +22,10 @@
         $from_address = 'info@example.com';
 
         //セパレートチェックボックスアイテム
-        $check = implode( ', ', $post['check'] );
+        $check = implode( ', ', $_POST['check'] );
 
         //自動返信
-        $to1 = $post['email'];
+        $to1 = $_POST['email'];
         $from1 = "From: ".$from_name.$from_address;
         $subject1 = 'お問い合わせありがとうございます';
         $body1 = <<<EOT
@@ -35,17 +35,17 @@
         以下の内容で送信いたしました。
         ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
-        名前： {$post['name']}
-        ふりがな： {$post['kana']}
-        メールアドレス： {$post['email']}
-        郵便番号： {$post['zipcode']}
-        住所1： {$post['address']}
-        住所2： {$post['address2']}
-        電話番号： {$post['tel']}
-        好きな色： {$post['select']}
+        名前： {$_POST['namae']}
+        ふりがな： {$_POST['kana']}
+        メールアドレス： {$_POST['email']}
+        郵便番号： {$_POST['zipcode']}
+        住所1： {$_POST['address']}
+        住所2： {$_POST['address2']}
+        電話番号： {$_POST['tel']}
+        好きな色： {$_POST['select']}
         好きなお寿司： {$check}
-        今日の天気： {$post['radio']}
-        問い合わせ内容： {$post['message']}
+        今日の天気： {$_POST['radio']}
+        問い合わせ内容： {$_POST['message']}
 
         ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
@@ -62,24 +62,24 @@
 
 
         //受付メール
-        $to2 = 'info@example.com';
-        $from2 = 'From: '.$post['email'];
+        $to2 = 'example@***.com';
+        $from2 = 'From: '.$_POST['email'];
         $subject2 = 'ホームページよりお問合せを受け付けました。';
         $body2 = <<<EOT
         お問い合わせ内容
         ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
-        名前： {$post['name']}
-        ふりがな： {$post['kana']}
-        メールアドレス： {$post['email']}
-        郵便番号： {$post['zipcode']}
-        住所1： {$post['address']}
-        住所2： {$post['address2']}
-        電話番号： {$post['tel']}
-        好きな色： {$post['select']}
+        名前： {$_POST['namae']}
+        ふりがな： {$_POST['kana']}
+        メールアドレス： {$_POST['email']}
+        郵便番号： {$_POST['zipcode']}
+        住所1： {$_POST['address']}
+        住所2： {$_POST['address2']}
+        電話番号： {$_POST['tel']}
+        好きな色： {$_POST['select']}
         好きなお寿司： {$check}
-        今日の天気： {$post['radio']}
-        問い合わせ内容： {$post['message']}
+        今日の天気： {$_POST['radio']}
+        問い合わせ内容： {$_POST['message']}
 
         ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
         EOT;
@@ -116,49 +116,49 @@
                 <div class="wrap__inner">
                     <div class="wrap__item">
                         <label>お名前</label>
-                        <p><?php echo htmlspecialchars($post['name']); ?></p>
+                        <p><?php echo htmlspecialchars(@$_POST['namae']); ?></p>
                     </div>
                     <div class="wrap__item">
                         <label>ふりがな</label>
-                        <p><?php echo htmlspecialchars($post['kana']); ?></p>
+                        <p><?php echo htmlspecialchars(@$_POST['kana']); ?></p>
                     </div>
                     <div class="wrap__item">
                         <label>Eメールアドレス</label>
-                        <p><?php echo htmlspecialchars($post['email']); ?></p>
+                        <p><?php echo htmlspecialchars(@$_POST['email']); ?></p>
                     </div>
                     <div class="wrap__item">
                         <label>郵便番号</label>
-                        <p><?php echo htmlspecialchars($post['zipcode']); ?></p>
+                        <p><?php echo htmlspecialchars(@$_POST['zipcode']); ?></p>
                     </div>
                     <div class="wrap__item">
                         <label>ご住所</label>
-                        <p><?php echo htmlspecialchars($post['address']); ?></p>
+                        <p><?php echo htmlspecialchars(@$_POST['address']); ?></p>
                     </div>
                     <div class="wrap__item">
                         <label>建物名</label>
-                        <p><?php echo htmlspecialchars($post['address2']); ?></p>
+                        <p><?php echo htmlspecialchars(@$_POST['address2']); ?></p>
                     </div>
                     <div class="wrap__item">
                         <label>電話番号</label>
-                        <p><?php echo htmlspecialchars($post['tel']); ?></p>
+                        <p><?php echo htmlspecialchars(@$_POST['tel']); ?></p>
                     </div>
                     <div class="wrap__item">
                         <label>好きな色</label>
-                        <p><?php echo htmlspecialchars($post['select']); ?></p>
+                        <p><?php echo htmlspecialchars(@$_POST['select']); ?></p>
                     </div>
                     <div class="wrap__item">
                         <label>好きなお寿司</label>
-                        <?php for($i = 0; $i < count($post['check']); $i++) : ?>
-                        <p><?php echo htmlspecialchars($post['check'][$i]); ?></p>
+                        <?php for($i = 0; $i < count($_POST['check']); $i++) : ?>
+                        <p><?php echo htmlspecialchars(@$_POST['check'][$i]); ?></p>
                         <?php endfor;?>
                     </div>
                     <div class="wrap__item">
                         <label>今日の天気</label>
-                        <p><?php echo htmlspecialchars($post['radio']); ?></p>
+                        <p><?php echo htmlspecialchars(@$_POST['radio']); ?></p>
                     </div>
                     <div class="wrap__item">
                         <label>お問い合わせの概要</label>
-                        <p><?php echo nl2br(htmlspecialchars($post['message'])); ?></p>
+                        <p><?php echo nl2br(htmlspecialchars(@$_POST['message'])); ?></p>
                     </div>
                 </div>
                 <a class="form__button button-gray" href="index.php">入力内容を変更する</a>
--- index.php
+++ index.php
@@ -4,51 +4,51 @@
     $errMessage = []; //エラーメッセージ配列の初期化
 
     if($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $post = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+        $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
         //フォームの送信時にエラーをチェックする
-        if($post['name'] === '') {
-            $errMessage['name'] = 'blank';
+        if($_POST['namae'] === '') {
+            $errMessage['namae'] = 'blank';
         }
-        if($post['kana'] === '') {
+        if($_POST['kana'] === '') {
             $errMessage['kana'] = 'blank';
         }
-        if($post['email'] === '') {
+        if($_POST['email'] === '') {
             $errMessage['email'] = 'blank';
-        }else if(!filter_var($post['email'], FILTER_VALIDATE_EMAIL)) {
+        }else if(!filter_var(@$_POST['email'], FILTER_VALIDATE_EMAIL)) {
             $errMessage['email'] = 'checkEmail';
         }
-        if($post['zipcode'] === '') {
+        if($_POST['zipcode'] === '') {
             $errMessage['zipcode'] = 'blank';
         }
-        if($post['address'] === '') {
+        if($_POST['address'] === '') {
             $errMessage['address'] = 'blank';
         }
-        if($post['tel'] === '') {
+        if($_POST['tel'] === '') {
             $errMessage['tel'] = 'blank';
         }
-        if($post['select'] === 'defalut') { //selectの値がdefultだったら
+        if($_POST['select'] === 'default') { //selectの値がdefultだったら
             $errMessage['select'] = 'blank';
         }
-        if(empty($post['check'])) { //配列が空だったら
+        if(empty($_POST['check'])) { //配列が空だったら
             $errMessage['check'] = 'blank';
         }
-        if(empty($post['radio'])) {
+        if(empty($_POST['radio'])) {
             $errMessage['radio'] = 'blank';
         }
-        if($post['message'] === '') {
+        if($_POST['message'] === '') {
             $errMessage['message'] = 'blank';
         }
 
         if(count($errMessage) === 0) {
             //エラーメッセージの配列が空＝エラーがなかったら
-            $_SESSION['form'] = $post; //セッション変数にPOSTの内容を渡す
+            $_SESSION['form'] = $_POST; //セッション変数にPOSTの内容を渡す
             header('Location: confirm.php');
             exit();
         }
     }else {
         //confirmで戻るボタンを押した時に入力値を消さない処理
         if(isset($_SESSION['form'])) {
-            $post = $_SESSION['form'];
+            $_POST = $_SESSION['form'];
         }
     }
 ?>
@@ -66,18 +66,18 @@
     <main>
         <h2 class="page__ttl">メールフォーム</h2>
         <div class="contact__form">
-            <form action="" method="post" novalidate>
+            <form action="" method="POST" novalidate>
                 <div class="form__item input__text required">
                     <div class="c-item-top">
                         <span class="required__txt">必須</span>
-                        <label class="label" for="name">お名前</label>
+                        <label class="label" for="namae">お名前</label>
                         <p class="input__sample">（例）リトル ミィ</p>
                     </div>
-                    <input type="text" name="name" id="name" value="<?php echo htmlspecialchars($post['name']); ?>">
-                    <?php if($errMessage['name'] === 'blank') : ?>
-                    <div class="input__error">
-                        <p>名前を入力してください。</p>
-                    </div>
+                    <input type="text" name="namae" id="namae" value="<?php echo htmlspecialchars(isset($_POST['namae']) ? ($_POST['namae']) : null); ?>">
+                    <?php if(!empty($errMessage['namae'])) : ?>
+                        <div class="input__error">
+                            <p>名前を入力してください。</p>
+                        </div>
                     <?php endif; ?>
                 </div>
                 <div class="form__item input__text required">
@@ -86,11 +86,11 @@
                         <label class="label" for="kana">ふりがな</label>
                         <p class="input__sample">（例）りとる みぃ</p>
                     </div>
-                    <input type="text" name="kana" id="kana" value="<?php echo htmlspecialchars($post['kana']); ?>">
-                    <?php if($errMessage['kana'] === 'blank') : ?>
-                    <div class="input__error">
-                        <p>ふりがなを入力してください。</p>
-                    </div>
+                    <input type="text" name="kana" id="kana" value="<?php echo htmlspecialchars(isset($_POST['kana']) ? ($_POST['kana']) : null); ?>">
+                    <?php if(!empty($errMessage['kana'])) : ?>
+                        <div class="input__error">
+                            <p>ふりがなを入力してください。</p>
+                        </div>
                     <?php endif; ?>
                 </div>
                 <div class="form__item input__text required">
@@ -99,16 +99,16 @@
                         <label class="label" for="email">Eメールアドレス</label>
                         <p class="input__sample">（例）xxxxx@xx.xx</p>
                     </div>
-                    <input type="email" name="email" id="email" value="<?php echo htmlspecialchars($post['email']); ?>">
-                    <?php if($errMessage['email'] === 'blank') : ?>
-                    <div class="input__error">
-                        <p>Eメールアドレスを入力してください。</p>
-                    </div>
+                    <input type="email" name="email" id="email" value="<?php echo htmlspecialchars(isset($_POST['email']) ? ($_POST['email']) : null); ?>">
+                    <?php if(!empty($errMessage['email'])) : ?>
+                        <div class="input__error">
+                            <p>Eメールアドレスを入力してください。</p>
+                        </div>
                     <?php endif; ?>
-                    <?php if($errMessage['email'] === 'checkEmail') : ?>
-                    <div class="input__error">
-                    <p>正しいEメールアドレスを入力してください。</p>
-                    </div>
+                    <?php if(isset($errMessage['email']) === 'checkEmail') : ?>
+                        <div class="input__error">
+                            <p>正しいEメールアドレスを入力してください。</p>
+                        </div>
                     <?php endif; ?>
                 </div>
                 <div class="form__item input__text required">
@@ -117,11 +117,11 @@
                         <label class="label" for="zipcode">郵便番号</label>
                         <p class="input__sample">（例）4310431</p>
                     </div>
-                    <input type="text" name="zipcode" id="zipcode" maxlength="8" onKeyUp="AjaxZip3.zip2addr(this,'','address','address');" value="<?php echo htmlspecialchars($post['zipcode']); ?>">
-                    <?php if($errMessage['zipcode'] === 'blank') : ?>
-                    <div class="input__error">
-                        <p>郵便番号を入力してください。</p>
-                    </div>
+                    <input type="text" name="zipcode" id="zipcode" maxlength="8" onKeyUp="AjaxZip3.zip2addr(this,'','address','address');" value="<?php echo htmlspecialchars($_POST['zipcode']); ?>">
+                    <?php if(!empty($errMessage['zipcode'])) : ?>
+                        <div class="input__error">
+                            <p>郵便番号を入力してください。</p>
+                        </div>
                     <?php endif; ?>
                 </div>
                 <div class="form__item input__text required">
@@ -130,11 +130,11 @@
                         <label class="label" for="address">ご住所</label>
                         <p class="input__sample">（例）ムーミンバレー 2814</p>
                     </div>
-                    <input type="text" name="address" id="address" value="<?php echo htmlspecialchars($post['address']); ?>">
-                    <?php if($errMessage['address'] === 'blank') : ?>
-                    <div class="input__error">
-                        <p>住所を入力してください。</p>
-                    </div>
+                    <input type="text" name="address" id="address" value="<?php echo htmlspecialchars(isset($_POST['address']) ? ($_POST['address']) : null); ?>">
+                    <?php if(!empty($errMessage['address'])) : ?>
+                        <div class="input__error">
+                            <p>住所を入力してください。</p>
+                        </div>
                     <?php endif; ?>
                 </div>
                 <div class="form__item input__text required">
@@ -143,7 +143,7 @@
                         <label class="label" for="address2">建物名</label>
                         <p class="input__sample">（例）ニョロニョロビルディング 301</p>
                     </div>
-                    <input type="text" name="address2" id="address2" value="<?php echo htmlspecialchars($post['address2']); ?>">
+                    <input type="text" name="address2" id="address2" value="<?php echo htmlspecialchars(isset($_POST['address2']) ? ($_POST['address2']) : null); ?>">
                 </div>
                 <div class="form__item input__text required">
                     <div class="c-item-top">
@@ -151,11 +151,11 @@
                         <label class="label" for="tel">電話番号</label>
                         <p class="input__sample">（例）000-000-0000</p>
                     </div>
-                    <input type="tel" name="tel" id="tel" value="<?php echo htmlspecialchars($post['tel']); ?>">
-                    <?php if($errMessage['tel'] === 'blank') : ?>
-                    <div class="input__error">
-                        <p>電話番号を入力してください。</p>
-                    </div>
+                    <input type="tel" name="tel" id="tel" value="<?php echo htmlspecialchars(isset($_POST['tel']) ? ($_POST['tel']) : null); ?>">
+                    <?php if(!empty($errMessage['tel'])) : ?>
+                        <div class="input__error">
+                            <p>電話番号を入力してください。</p>
+                        </div>
                     <?php endif; ?>
                 </div>
                 <div class="form__item form__select required">
@@ -164,16 +164,16 @@
                         <label for="select">好きな色</label>
                     </div>
                     <select name="select" id="select">
-                        <option value="defalut">選択してください</option>
-                        <option value="赤" <?php if(isset($post['select']) && $post['select'] === '赤') echo 'selected'; ?>>赤</option>
-                        <option value="ピンク" <?php if(isset($post['select']) && $post['select'] === 'ピンク') echo 'selected'; ?>>ピンク</option>
-                        <option value="青" <?php if(isset($post['select']) && $post['select'] === '青') echo 'selected'; ?>>青</option>
-                        <option value="黄色" <?php if(isset($post['select']) && $post['select'] === '黄色') echo 'selected'; ?>>黄色</option>
+                        <option value="default">選択してください</option>
+                        <option value="赤" <?php if(isset($_POST['select']) && $_POST['select'] === '赤') echo 'selected'; ?>>赤</option>
+                        <option value="ピンク" <?php if(isset($_POST['select']) && $_POST['select'] === 'ピンク') echo 'selected'; ?>>ピンク</option>
+                        <option value="青" <?php if(isset($_POST['select']) && $_POST['select'] === '青') echo 'selected'; ?>>青</option>
+                        <option value="黄色" <?php if(isset($_POST['select']) && $_POST['select'] === '黄色') echo 'selected'; ?>>黄色</option>
                     </select>
-                    <?php if($errMessage['select'] === 'blank') : ?>
-                    <div class="input__error">
-                    <p>選択してください。</p>
-                    </div>
+                    <?php if(!empty($errMessage['select'])) : ?>
+                        <div class="input__error">
+                            <p>選択してください。</p>
+                        </div>
                     <?php endif; ?>
                 </div>
                 <div class="form__item form__check required">
@@ -181,13 +181,13 @@
                         <span class="required__txt">必須</span>
                         <label>好きなお寿司</label>
                     </div>
-                    <input type="checkbox" name="check[]" value="まぐろ" id="check1" <?php if(isset($post['check']) && in_array('まぐろ', $post['check'])) echo 'checked'; ?>><label for="check1">まぐろ</label>
-                    <input type="checkbox" name="check[]" value="たい" id="check2" <?php if(isset($post['check']) && in_array('たい', $post['check'])) echo 'checked'; ?>><label for="check2">たい</label>
-                    <input type="checkbox" name="check[]" value="たこ" id="check3" <?php if(isset($post['check']) && in_array('たこ', $post['check'])) echo 'checked'; ?>><label for="check3">たこ</label>
-                    <?php if($errMessage['check'] === 'blank') : ?>
-                    <div class="input__error">
-                    <p>チェックしてください。</p>
-                    </div>
+                    <input type="checkbox" name="check[]" value="まぐろ" id="check1" <?php if(isset($_POST['check']) && in_array('まぐろ', (array)$_POST['check'])) echo 'checked'; ?>><label for="check1">まぐろ</label>
+                    <input type="checkbox" name="check[]" value="たい" id="check2" <?php if(isset($_POST['check']) && in_array('たい', (array)$_POST['check'])) echo 'checked'; ?>><label for="check2">たい</label>
+                    <input type="checkbox" name="check[]" value="たこ" id="check3" <?php if(isset($_POST['check']) && in_array('たこ', (array)$_POST['check'])) echo 'checked'; ?>><label for="check3">たこ</label>
+                    <?php if(!empty($errMessage['check'])) : ?>
+                        <div class="input__error">
+                            <p>チェックしてください。</p>
+                        </div>
                     <?php endif; ?>
                 </div>
                 <div class="form__item form__radio required">
@@ -195,13 +195,13 @@
                         <span class="required__txt">必須</span>
                         <label>今日の天気</label>
                     </div>
-                    <input type="radio" name="radio" value="晴れ" id="radio1" <?php if(isset($post['radio']) && $post['radio'] === '晴れ') echo 'checked'; ?>><label for="radio1">晴れ</label>
-                    <input type="radio" name="radio" value="くもり" id="radio2" <?php if(isset($post['radio']) && $post['radio'] === 'くもり') echo 'checked'; ?>><label for="radio2">くもり</label>
-                    <input type="radio" name="radio" value="雨" id="radio3" <?php if(isset($post['radio']) && $post['radio'] === '雨') echo 'checked'; ?>><label for="radio3">雨</label>
-                    <?php if($errMessage['radio'] === 'blank') : ?>
-                    <div class="input__error">
-                    <p>チェックしてください。</p>
-                    </div>
+                    <input type="radio" name="radio" value="晴れ" id="radio1" <?php if(isset($_POST['radio']) && $_POST['radio'] === '晴れ') echo 'checked'; ?>><label for="radio1">晴れ</label>
+                    <input type="radio" name="radio" value="くもり" id="radio2" <?php if(isset($_POST['radio']) && $_POST['radio'] === 'くもり') echo 'checked'; ?>><label for="radio2">くもり</label>
+                    <input type="radio" name="radio" value="雨" id="radio3" <?php if(isset($_POST['radio']) && $_POST['radio'] === '雨') echo 'checked'; ?>><label for="radio3">雨</label>
+                    <?php if(!empty($errMessage['radio'])) : ?>
+                        <div class="input__error">
+                            <p>チェックしてください。</p>
+                        </div>
                     <?php endif; ?>
                 </div>
                 <div class="form__item form__message required">
@@ -209,14 +209,14 @@
                         <span class="required__txt">必須</span>
                         <label for="message">ミィへのメッセージ</label>
                     </div>
-                    <textarea name="message" id="message"><?php echo htmlspecialchars($post['message']); ?></textarea>
-                    <?php if($errMessage['message'] === 'blank') : ?>
-                    <div class="input__error">
-                        <p>お問い合わせの概要をご記入ください。</p>
-                    </div>
+                    <textarea name="message" id="message"><?php echo nl2br(htmlspecialchars(isset($_POST['message']) ? ($_POST['message']) : null)); ?></textarea>
+                    <?php if(!empty($errMessage['message'])) : ?>
+                        <div class="input__error">
+                            <p>お問い合わせの概要をご記入ください。</p>
+                        </div>
                     <?php endif; ?>
                 </div>
-                <button type="submit" class="form__button button-blue" href="javascript:void(0)">確認画面へ</button>
+                <button type="submit" class="form__button button-blue">確認画面へ</button>
             </form>
         </div>
     </main>
